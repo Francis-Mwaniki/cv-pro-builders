cd7e3c9c62bc0f61f414d187d963a52fca8b89b1
Franc-dev <francismwanik254@gmail.com>
2024-11-16 11:06:08 +0300

    cv builder

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,27 @@ const quicksand = Quicksand({
 })
 
 
+
 export const metadata: Metadata = {
-  title: "cv builder pro",
-  description: "build Your CV is seconds",
-};
+  title: 'CV Builder - Create Your Professional Resume',
+  description: 'Build and customize your professional CV with our easy-to-use online CV builder. Stand out from the crowd and land your dream job.',
+  openGraph: {
+    title: 'CV Builder - Create Your Professional Resume',
+    description: 'Build and customize your professional CV with our easy-to-use online CV builder. Stand out from the crowd and land your dream job.',
+    images: ['/og-image.png'],
+    url: 'https://cv-pro-builders.vercel.app',
+    siteName: 'CV Builder',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CV Builder - Create Your Professional Resume',
+    description: 'Build and customize your professional CV with our easy-to-use online CV builder. Stand out from the crowd and land your dream job.',
+    images: ['/og-image.png'],
+    creator: '@FRANCIS90776084',
+  },
+}
 
 export default function RootLayout({
   children,
